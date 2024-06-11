abb1e6beccbb34489dfcb4804a35b46f859a02b4
Mari <chl98639863@gmail.com>
2024-06-11 09:21:57 +0900

    Add files via upload

--- lab05_jsp/src/main/java/com/itwill/lab05/datasource/DataSourceUtil.java
+++ lab05_jsp/src/main/java/com/itwill/lab05/datasource/DataSourceUtil.java
@@ -13,15 +13,15 @@ public class DataSourceUtil {
 	
 	private DataSourceUtil() {
 		// HikariCP의 설정(configutation)을 담당하는 객체.
-		config = new HikariConfig(); 
+		config = new HikariConfig(); //기본생성자 호출하며 객체 생성함
 		
-		//커넥션 풀(데이터소스) 환경 설정.
+		//커넥션 풀(데이터소스) 환경 설정. setter메서드 호출
 		config.setDriverClassName("oracle.jdbc.OracleDriver");
 		config.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:xe");
 		config.setUsername("jspstudy"); //오라클 DB 계정의 아이디
 		config.setPassword("jspstudy"); //오라클 DB 계정의 비밀번호
 		
-		//데이터 소스 객체 생성. 
+		//데이터 소스 객체 생성. 데이터 소스에서 커넥션 빌려서 씀
 		ds = new HikariDataSource(config);
 	}
 	
